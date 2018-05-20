026cca1ced12626c3d385778a63b119938078f51
Satyendra_Kumar_Banjare <satyendrabanjare99@gmail.com>
2018-05-21 01:29:07 +0530

    linked config functions to main wrapper

--- src/main_wrapper.c
+++ src/main_wrapper.c
@@ -1,5 +1,6 @@
 #include "./header_files/lua_libgit2.h"
 #include "./header_files/lua_cred.h"
+#include "./header_files/lua_config.h"
 
 static const struct luaL_Reg luagit2 [] = {
 
@@ -39,6 +40,35 @@ static const struct luaL_Reg luagit2 [] = {
       {"luagit2_cred_username_new",lua_git_cred_username_new},
       {"luagit2_cred_userpass_plaintext_new",lua_git_cred_userpass_plaintext_new},
       //---------------------------------------------------------------------------------------------//
+
+      //----------------------------- config functions ----------------------------------------------//
+      {"luagit2_config_delete_entry",lua_git_config_delete_entry},
+      {"luagit2_config_delete_multivar",lua_git_config_delete_multivar},
+      {"luagit2_config_find_global",lua_git_config_find_global},
+      {"luagit2_config_find_programdata",lua_git_config_find_programdata},
+      {"luagit2_config_find_system",lua_git_config_find_system},
+      {"luagit2_config_find_xdg",lua_git_config_find_xdg},
+      {"luagit2_config_get_bool",lua_git_config_get_bool},
+      {"luagit2_config_get_int32",lua_git_config_get_int32},
+      {"luagit2_config_get_int64",lua_git_config_get_int64},
+      {"luagit2_config_get_path",lua_git_config_get_path},
+      {"luagit2_config_get_string",lua_git_config_get_string},
+      {"luagit2_config_get_string_buf",lua_git_config_get_string_buf},
+      {"luagit2_config_open_global",lua_git_config_open_global},
+      {"luagit2_config_open_level",lua_git_config_open_level},
+      {"luagit2_config_open_ondisk",lua_git_config_open_ondisk},
+      {"luagit2_config_open_default",lua_git_config_open_default},
+      {"luagit2_config_parse_bool",lua_git_config_parse_bool},
+      {"luagit2_config_parse_int32",lua_git_config_parse_int32},
+      {"luagit2_config_parse_int64",lua_git_config_parse_int64},
+      {"luagit2_config_parse_path",lua_git_config_parse_path},
+      {"luagit2_config_set_bool",lua_git_config_set_bool},
+      {"luagit2_config_set_int32",lua_git_config_set_int32},
+      {"luagit2_config_set_int64",lua_git_config_set_int64},
+      {"luagit2_config_set_multivar",lua_git_config_set_multivar},
+      {"luagit2_config_set_string",lua_git_config_set_string},
+      {"luagit2_config_snapshot",lua_git_config_snapshot},
+      //----------------------------------------------------------------------------------------------//
 
       {NULL, NULL}  
     };
