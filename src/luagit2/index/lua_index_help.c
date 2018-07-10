8abd8dc2e6d46e4b628c81d7b5d83f9f5828e029
Satyendra_Kumar_Banjare <satyendrabanjare99@gmail.com>
2018-07-10 19:26:56 +0530

    Refactor : Modified signature module source + tests
    Also slightly modified index module helper function

--- src/luagit2/index/lua_index_help.c
+++ src/luagit2/index/lua_index_help.c
@@ -5,7 +5,7 @@ int lua_git_index_entry_get_path(lua_State *L) {
 	if ((Index_entry->index_entry) != NULL) {
 		lua_pushstring(L, (Index_entry->index_entry)->path);
 	} else {
-		lua_pushstring(L, null_err);
+		luaL_error(L, null_err);
 	}
 	return 1;
 }
@@ -15,7 +15,7 @@ int lua_git_index_entry_get_filemode(lua_State *L) {
 	if ((Index_entry->index_entry) != NULL) {
 		lua_pushinteger(L, (int)(Index_entry->index_entry)->mode);
 	} else {
-		lua_pushstring(L, null_err);
+		luaL_error(L, null_err);
 	}
 	return 1;
 }
@@ -25,7 +25,7 @@ int lua_git_index_entry_get_filesize(lua_State *L) {
 	if ((Index_entry->index_entry) != NULL) {
 		lua_pushinteger(L, (int)(Index_entry->index_entry)->file_size);
 	} else {
-		lua_pushstring(L, null_err);
+		luaL_error(L, null_err);
 	}
 	return 1;
 }
@@ -35,7 +35,7 @@ int lua_git_index_entry_get_stage(lua_State *L) {
 	if ((Index_entry->index_entry) != NULL) {
 		lua_pushinteger(L, git_index_entry_stage(Index_entry->index_entry));
 	} else {
-		lua_pushstring(L, null_err);
+		luaL_error(L, null_err);
 	}
 	return 1;
 }
@@ -48,7 +48,7 @@ int lua_git_index_entry_get_oid_str(lua_State *L) {
 		git_oid_fmt(out, &(Index_entry->index_entry)->id);
 		lua_pushstring(L, out);
 	} else {
-		lua_pushstring(L, null_err);
+		luaL_error(L, null_err);
 	}
 	return 1;
 }
@@ -59,7 +59,7 @@ int lua_git_index_entry_get_dev_inode(lua_State *L) {
 		lua_pushinteger(L, (Index_entry->index_entry)->dev);
 		lua_pushinteger(L, (Index_entry->index_entry)->ino);
 	} else {
-		lua_pushstring(L, null_err);
+		luaL_error(L, null_err);
 	}
 	return 2;
 
@@ -71,7 +71,7 @@ int lua_git_index_entry_get_UID_GID(lua_State *L) {
 		lua_pushinteger(L, (Index_entry->index_entry)->uid);
 		lua_pushinteger(L, (Index_entry->index_entry)->gid);
 	} else {
-		lua_pushstring(L, null_err);
+		luaL_error(L, null_err);
 	}
 	return 2;
 }
--- src/luagit2/signature/lua_signature_help.c
+++ src/luagit2/signature/lua_signature_help.c
@@ -1,12 +1,12 @@
 #include "lua_signature_help.h"
 
-static void print_signature( const git_signature *sig)
-{
+static void print_signature( const git_signature *sig) {
 	char sign;
 	int offset, hours, minutes;
 
-	if (!sig)
+	if (!sig) {
 		return;
+	}
 
 	offset = sig->when.offset;
 	if (offset < 0) {
@@ -20,27 +20,41 @@ static void print_signature( const git_signature *sig)
 	minutes = offset % 60;
 
 	printf("%s <%s> %ld %c%02d%02d\n",
-		   sig->name, sig->email, (long)sig->when.time,
-		   sign, hours, minutes);
+	    sig->name, sig->email, (long)sig->when.time,
+	    sign, hours, minutes);
 }
 
 int lua_get_signature_details (lua_State *L) {
-	const luagit2_signature *signature_data = (luagit2_signature *)lua_touserdata(L, 1);
-	int which_value = luaL_checkinteger(L,2);
 
-	switch(which_value)
-	{
-		case 1:  lua_pushstring(L,signature_data->sign->name);
-		    	break;
-		case 2: lua_pushstring(L,signature_data->sign->email);
-		    	break;
+	if (lua_gettop(L) != 1) {
+		return luaL_error(L, "expecting exactly 1 argument : luagit2_signature");
 	}
 
-    return 1;
+	const luagit2_signature *signature_data = (luagit2_signature *)luaL_checkudata(L, 1, "luagit2_signature");
+
+	if (signature_data->sign != NULL) {
+		lua_pushstring(L, signature_data->sign->name);
+		lua_pushstring(L, signature_data->sign->email);
+		return 2;
+	} else {
+		return luaL_error(L, null_err);
+	}
+
 }
 
 int lua_print_complete_signature_details (lua_State *L) {
-	const luagit2_signature *signature_data = (luagit2_signature *)lua_touserdata(L, 1);
-	print_signature(signature_data->sign);
-    return 1;
+
+	if (lua_gettop(L) != 1) {
+		return luaL_error(L, "expecting exactly 1 argument : luagit2_signature");
+	}
+
+	const luagit2_signature *signature_data = (luagit2_signature *)luaL_checkudata(L, 1, "luagit2_signature");
+
+	if (signature_data->sign != NULL) {
+		print_signature(signature_data->sign);
+		return 1;
+	} else {
+		return luaL_error(L, null_err);
+	}
+
 }
