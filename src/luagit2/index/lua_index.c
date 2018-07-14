e14d6235727d0d92b46df2412e726cb68770aef5
Satyendra_Kumar_Banjare <satyendrabanjare99@gmail.com>
2018-07-14 09:52:13 +0530

    refactor : index module source + tests

--- src/luagit2/index/lua_index.c
+++ src/luagit2/index/lua_index.c
@@ -1,8 +1,13 @@
 #include "lua_index.h"
 
 int lua_git_index_add(lua_State *L) {
-	luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
-	const luagit2_index_entry *Index_entry = (luagit2_index_entry *)lua_touserdata(L, 2);
+
+	if (lua_gettop(L) != 2) {
+		return luaL_error(L, "expecting exactly 2 arguments : luagit2_index, luagit2_index_entry");
+	}
+
+	luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
+	const luagit2_index_entry *Index_entry = (luagit2_index_entry *)luaL_checkudata(L, 2, "luagit2_index_entry");
 
 	check_error_long(git_index_add(Index->index, Index_entry->index_entry),
 	    "Unable to add/update index entry", NULL);
@@ -12,7 +17,12 @@ int lua_git_index_add(lua_State *L) {
 }
 
 int lua_git_index_add_bypath(lua_State *L) {
-	luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 2) {
+		return luaL_error(L, "expecting exactly 2 arguments : luagit2_index, (string)path");
+	}
+
+	luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 	const char *path = luaL_checkstring(L, 2);
 
 	check_error_long(git_index_add_bypath(Index->index, path),
@@ -23,7 +33,12 @@ int lua_git_index_add_bypath(lua_State *L) {
 }
 
 int lua_git_index_caps(lua_State *L) {
-	luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 1) {
+		return luaL_error(L, "expecting exactly 1 argument : luagit2_index");
+	}
+
+	luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 
 	int values = git_index_caps(Index->index);
 	lua_pushinteger(L, values);
@@ -33,8 +48,13 @@ int lua_git_index_caps(lua_State *L) {
 }
 
 int lua_git_index_checksum(lua_State *L) {
+
+	if (lua_gettop(L) != 1) {
+		return luaL_error(L, "expecting exactly 1 argument : luagit2_index");
+	}
+
 	luagit2_oid *obj_id;
-	const luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+	const luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 
 	obj_id = (luagit2_oid *)lua_newuserdata(L, sizeof(*obj_id));
 
@@ -48,10 +68,16 @@ int lua_git_index_checksum(lua_State *L) {
 }
 
 int lua_git_index_conflict_add(lua_State *L) {
-	luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
-	const luagit2_index_entry *Index_ancestor_entry = (luagit2_index_entry *)lua_touserdata(L, 2);
-	const luagit2_index_entry *Index_our_entry = (luagit2_index_entry *)lua_touserdata(L, 3);
-	const luagit2_index_entry *Index_their_entry = (luagit2_index_entry *)lua_touserdata(L, 4);
+
+	if (lua_gettop(L) != 4) {
+		return luaL_error(L, "expecting exactly 1 argument : luagit2_index,(luagit2_index_entry)ancestor,\
+			(luagit2_index_entry)ours,(luagit2_index_entry)theirs");
+	}
+
+	luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
+	const luagit2_index_entry *Index_ancestor_entry = (luagit2_index_entry *)luaL_checkudata(L, 2, "luagit2_index_entry");
+	const luagit2_index_entry *Index_our_entry = (luagit2_index_entry *)luaL_checkudata(L, 3, "luagit2_index_entry");
+	const luagit2_index_entry *Index_their_entry = (luagit2_index_entry *)luaL_checkudata(L, 4, "luagit2_index_entry");
 
 	check_error_long(git_index_conflict_add(Index->index, Index_ancestor_entry->index_entry
 	        , Index_our_entry->index_entry, Index_their_entry->index_entry),
@@ -62,7 +88,12 @@ int lua_git_index_conflict_add(lua_State *L) {
 }
 
 int lua_git_index_conflict_cleanup(lua_State *L) {
-	luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 1) {
+		return luaL_error(L, "expecting exactly 1 argument : luagit2_index");
+	}
+
+	luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 
 	git_index_conflict_cleanup(Index->index);
 
@@ -71,7 +102,12 @@ int lua_git_index_conflict_cleanup(lua_State *L) {
 }
 
 int lua_git_index_entry_is_conflict(lua_State *L) {
-	luagit2_index_entry *Index_entry = (luagit2_index_entry *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 1) {
+		return luaL_error(L, "expecting exactly 1 argument : luagit2_index_entry");
+	}
+
+	luagit2_index_entry *Index_entry = (luagit2_index_entry *)luaL_checkudata(L, 1, "luagit2_index_entry");
 
 	int is_conflict = git_index_entry_is_conflict(Index_entry->index_entry);
 	lua_pushinteger(L, is_conflict);
@@ -81,7 +117,12 @@ int lua_git_index_entry_is_conflict(lua_State *L) {
 }
 
 int lua_git_index_entry_stage(lua_State *L) {
-	luagit2_index_entry *Index_entry = (luagit2_index_entry *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 1) {
+		return luaL_error(L, "expecting exactly 1 argument : luagit2_index_entry");
+	}
+
+	luagit2_index_entry *Index_entry = (luagit2_index_entry *)luaL_checkudata(L, 1, "luagit2_index_entry");
 
 	int stage_value = git_index_entry_stage(Index_entry->index_entry);
 	lua_pushinteger(L, stage_value);
@@ -91,7 +132,12 @@ int lua_git_index_entry_stage(lua_State *L) {
 }
 
 int lua_git_index_entrycount(lua_State *L) {
-	luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 1) {
+		return luaL_error(L, "expecting exactly 1 argument : luagit2_index");
+	}
+
+	luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 
 	int index_entry_count = git_index_entrycount(Index->index);
 	lua_pushinteger(L, index_entry_count);
@@ -101,7 +147,12 @@ int lua_git_index_entrycount(lua_State *L) {
 }
 
 int lua_git_index_find(lua_State *L) {
-	luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 2) {
+		return luaL_error(L, "expecting exactly 2 arguments : luagit2_index,(string)path");
+	}
+
+	luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 	const char *path = luaL_checkstring(L, 2);
 
 	size_t at_pos;
@@ -113,7 +164,12 @@ int lua_git_index_find(lua_State *L) {
 }
 
 int lua_git_index_find_prefix(lua_State *L) {
-	luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 2) {
+		return luaL_error(L, "expecting exactly 2 arguments : luagit2_index,(string)prefix");
+	}
+
+	luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 	const char *prefix = luaL_checkstring(L, 2);
 
 	size_t at_pos;
@@ -125,7 +181,12 @@ int lua_git_index_find_prefix(lua_State *L) {
 }
 
 int lua_git_index_get_byindex(lua_State *L) {
-	luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 2) {
+		return luaL_error(L, "expecting exactly 2 arguments : luagit2_index,(integer)index_value");
+	}
+
+	luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 	size_t n = luaL_checkinteger(L, 2);
 
 	luagit2_index_entry *Index_entry;
@@ -141,7 +202,12 @@ int lua_git_index_get_byindex(lua_State *L) {
 }
 
 int lua_git_index_get_bypath(lua_State *L) {
-	const luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 3) {
+		return luaL_error(L, "expecting exactly 3 arguments : luagit2_index,(string)path,(integer)stage_value");
+	}
+
+	const luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 	const char *path = luaL_checkstring(L, 2);
 	int stage = luaL_checkinteger(L, 3);
 
@@ -158,7 +224,12 @@ int lua_git_index_get_bypath(lua_State *L) {
 }
 
 int lua_git_index_has_conflicts(lua_State *L) {
-	const luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 1) {
+		return luaL_error(L, "expecting exactly 1 argument : luagit2_index");
+	}
+
+	const luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 
 	int has_conflicts = git_index_has_conflicts(Index->index);
 	lua_pushinteger(L, has_conflicts);
@@ -168,6 +239,11 @@ int lua_git_index_has_conflicts(lua_State *L) {
 }
 
 int lua_git_index_open(lua_State *L) {
+
+	if (lua_gettop(L) != 1) {
+		return luaL_error(L, "expecting exactly 1 argument : (string)index_file_path");
+	}
+
 	const char *path = luaL_checkstring(L, 1);
 
 	luagit2_index *lua_index;
@@ -185,8 +261,13 @@ int lua_git_index_open(lua_State *L) {
 }
 
 int lua_git_index_owner (lua_State *L) {
+
+	if (lua_gettop(L) != 1) {
+		return luaL_error(L, "expecting exactly 1 argument : luagit2_index");
+	}
+
 	luagit2_repository *lua_repo;
-	const luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+	const luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 
 	lua_repo = (luagit2_repository *)lua_newuserdata(L, sizeof(*lua_repo));
 	lua_repo->repo  = NULL;
@@ -199,7 +280,12 @@ int lua_git_index_owner (lua_State *L) {
 }
 
 int lua_git_index_path(lua_State *L) {
-	const luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 1) {
+		return luaL_error(L, "expecting exactly 1 argument : luagit2_index");
+	}
+
+	const luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 	char *index_path = git_index_path(Index->index);
 	lua_pushstring(L, index_path);
 
@@ -208,7 +294,12 @@ int lua_git_index_path(lua_State *L) {
 }
 
 int lua_git_index_read(lua_State *L) {
-	const luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 2) {
+		return luaL_error(L, "expecting exactly 2 argument : luagit2_index,(integer)force");
+	}
+
+	const luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 	int force = luaL_checkinteger(L, 2);
 	check_error_long(git_index_read(Index->index, force),
 	    "Unable to read from hard disk to update index", NULL);
@@ -218,8 +309,13 @@ int lua_git_index_read(lua_State *L) {
 }
 
 int lua_git_index_read_tree(lua_State *L) {
-	const luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
-	const luagit2_tree *Tree = (luagit2_tree *)lua_touserdata(L, 2);
+
+	if (lua_gettop(L) != 2) {
+		return luaL_error(L, "expecting exactly 2 arguments : luagit2_index,luagit2_tree");
+	}
+
+	const luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
+	const luagit2_tree *Tree = (luagit2_tree *)luaL_checkudata(L, 2, "luagit2_tree");
 	check_error_long(git_index_read_tree(Index->index, Tree->tree),
 	    "Unable to read a tree into index file", NULL);
 
@@ -228,7 +324,12 @@ int lua_git_index_read_tree(lua_State *L) {
 }
 
 int lua_git_index_remove(lua_State *L) {
-	const luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 3) {
+		return luaL_error(L, "expecting exactly 3 arguments : luagit2_index,(string)path,(integer)stage_value");
+	}
+
+	const luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 	const char *path = luaL_checkstring(L, 2);
 	int stage = luaL_checkinteger(L, 3);
 
@@ -239,7 +340,12 @@ int lua_git_index_remove(lua_State *L) {
 }
 
 int lua_git_index_remove_bypath(lua_State *L) {
-	const luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 2) {
+		return luaL_error(L, "expecting exactly 2 arguments : luagit2_index,(string)path");
+	}
+
+	const luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 	const char *path = luaL_checkstring(L, 2);
 
 	check_error_long(git_index_remove_bypath(Index->index, path),
@@ -249,7 +355,12 @@ int lua_git_index_remove_bypath(lua_State *L) {
 }
 
 int lua_git_index_remove_directory(lua_State *L) {
-	const luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 3) {
+		return luaL_error(L, "expecting exactly 3 arguments : luagit2_index,(string)path,(integer)stage_value");
+	}
+
+	const luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 	const char *dir_path = luaL_checkstring(L, 2);
 	int stage = luaL_checkinteger(L, 3);
 
@@ -260,7 +371,12 @@ int lua_git_index_remove_directory(lua_State *L) {
 }
 
 int lua_git_index_set_caps(lua_State *L) {
-	const luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 2) {
+		return luaL_error(L, "expecting exactly 2 arguments : luagit2_index,(integer)caps");
+	}
+
+	const luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 	int caps = luaL_checkinteger(L, 2);
 
 	check_error_long(git_index_set_caps(Index->index, caps),
@@ -271,7 +387,12 @@ int lua_git_index_set_caps(lua_State *L) {
 
 
 int lua_git_index_set_version(lua_State *L) {
-	const luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 2) {
+		return luaL_error(L, "expecting exactly 2 arguments : luagit2_index,(integer)version");
+	}
+
+	const luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 	unsigned int version = luaL_checkinteger(L, 2);
 
 	check_error_long(git_index_set_version(Index->index, version),
@@ -281,7 +402,12 @@ int lua_git_index_set_version(lua_State *L) {
 }
 
 int lua_git_index_version(lua_State *L) {
-	const luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 1) {
+		return luaL_error(L, "expecting exactly 1 argument : luagit2_index");
+	}
+
+	const luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 
 	unsigned int version_number = git_index_version(Index->index);
 	lua_pushnumber(L, version_number);
@@ -290,7 +416,12 @@ int lua_git_index_version(lua_State *L) {
 }
 
 int lua_git_index_write(lua_State *L) {
-	const luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 1) {
+		return luaL_error(L, "expecting exactly 1 argument : luagit2_index");
+	}
+
+	const luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 
 	check_error_long(git_index_write(Index->index),
 	    "Unable to write to given index", NULL);
@@ -299,8 +430,13 @@ int lua_git_index_write(lua_State *L) {
 }
 
 int lua_git_index_write_tree(lua_State *L) {
+
+	if (lua_gettop(L) != 1) {
+		return luaL_error(L, "expecting exactly 1 argument : luagit2_index");
+	}
+
 	luagit2_oid *obj_id;
-	const luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+	const luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 
 	obj_id = (luagit2_oid *)lua_newuserdata(L, sizeof(*obj_id));
 
@@ -317,9 +453,14 @@ int lua_git_index_write_tree(lua_State *L) {
 }
 
 int lua_git_index_write_tree_to(lua_State *L) {
+
+	if (lua_gettop(L) != 2) {
+		return luaL_error(L, "expecting exactly 2 arguments : luagit2_repository");
+	}
+
 	luagit2_oid *obj_id;
-	const luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
-	const luagit2_repository *Repo = (luagit2_repository *)lua_touserdata(L, 2);
+	const luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
+	const luagit2_repository *Repo = (luagit2_repository *)luaL_checkudata(L, 2, "luagit2_repository");
 
 	obj_id = (luagit2_oid *)lua_newuserdata(L, sizeof(*obj_id));
 
@@ -336,7 +477,12 @@ int lua_git_index_write_tree_to(lua_State *L) {
 }
 
 int lua_git_index_free(lua_State *L) {
-	luagit2_index *Index = (luagit2_index *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 1) {
+		return luaL_error(L, "expecting exactly 1 argument : luagit2_index");
+	}
+
+	luagit2_index *Index = (luagit2_index *)luaL_checkudata(L, 1, "luagit2_index");
 	git_index_free( Index->index);
 
 	return 1;
