d5f69c09d8e7b35eb0f8545ac0a13ec31c1d8e53
Satyendra_Kumar_Banjare <satyendrabanjare99@gmail.com>
2018-07-26 10:34:45 +0530

    Fixed some errors, modified tests & removed unnecessary comments

--- src/luagit2/config/lua_config_help.c
+++ src/luagit2/config/lua_config_help.c
@@ -1,35 +1,5 @@
 #include "lua_config_help.h"
 
-// typedef enum {
-// 	/** System-wide on Windows, for compatibility with portable git */
-// 	GIT_CONFIG_LEVEL_PROGRAMDATA = 1,
-
-// 	/** System-wide configuration file; /etc/gitconfig on Linux systems */
-// 	GIT_CONFIG_LEVEL_SYSTEM = 2,
-
-// 	/** XDG compatible configuration file; typically ~/.config/git/config */
-// 	GIT_CONFIG_LEVEL_XDG = 3,
-
-// 	/** User-specific configuration file (also called Global configuration
-// 	 * file); typically ~/.gitconfig
-// 	 */
-// 	GIT_CONFIG_LEVEL_GLOBAL = 4,
-
-// 	/** Repository specific configuration file; $WORK_DIR/.git/config on
-// 	 * non-bare repos
-// 	 */
-// 	GIT_CONFIG_LEVEL_LOCAL = 5,
-
-// 	/** Application specific configuration file; freely defined by applications
-// 	 */
-// 	GIT_CONFIG_LEVEL_APP = 6,
-
-// 	/** Represents the highest level available config file (i.e. the most
-// 	 * specific config file available that actually is loaded)
-// 	 */
-// 	GIT_CONFIG_HIGHEST_LEVEL = -1,
-// } git_config_level_t;
-
 int lua_get_config_level_program_data(lua_State *L) {
 	luagit2_config_level_t *Cfg_lvl ;
 	Cfg_lvl = (luagit2_config_level_t *)lua_newuserdata(L, sizeof(*Cfg_lvl));
