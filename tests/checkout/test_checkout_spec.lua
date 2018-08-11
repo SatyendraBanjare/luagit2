e8857731bc1edac958dcdcea8bb92970999f6f77
Satyendra_Kumar_Banjare <satyendrabanjare99@gmail.com>
2018-08-11 19:54:00 +0530

    tests(checkout) : modified tests for coverage

--- tests/checkout/test_checkout_spec.lua
+++ tests/checkout/test_checkout_spec.lua
@@ -4,6 +4,7 @@ local fixer = require("Fixtures.fix_repo")
 
 describe(" Checkout Methods Tests ", function()
     local luagit2 = require("luagit2")
+    local luagit2_checkout = require("checkout.checkout_cover")
     local repo_path = "Fixtures/WORKON_REPO"
     
     setup(function()
@@ -25,19 +26,19 @@ describe(" Checkout Methods Tests ", function()
     end)
     
     it("Tests checkout master ", function ()
-        local checkout_init_opts = luagit2.checkout_init_options()
+        local checkout_init_opts = luagit2_checkout.checkout_init_options()
         -- checkout head
-        luagit2.checkout_head(repo, checkout_init_opts)
+        luagit2_checkout.checkout_head(repo, checkout_init_opts)
         local repo_HEAD_ref = luagit2.repository_head(repo)
         local ref_name = luagit2.reference_name(repo_HEAD_ref)
         assert.are.equal("refs/heads/master", ref_name)
     end)
     
     it("Tests checkout index ", function ()
-        local checkout_init_opts = luagit2.checkout_init_options()
+        local checkout_init_opts = luagit2_checkout.checkout_init_options()
         -- checkout current index (by default it is master)
         local index_open_by_path = luagit2.index_open("/Fixtures/WORKON_REPO/.git/index")
-        luagit2.checkout_index(repo, index_open_by_path, checkout_init_opts)
+        luagit2_checkout.checkout_index(repo, index_open_by_path, checkout_init_opts)
         local repo_HEAD_ref = luagit2.repository_head(repo)
         local ref_name = luagit2.reference_name(repo_HEAD_ref)
         assert.are.equal("refs/heads/master", ref_name)
@@ -65,14 +66,14 @@ describe(" Checkout Methods Tests ", function()
 
     it("Tests checkout tree ", function ()
         -- Setup the variables
-        local checkout_init_opts = luagit2.checkout_init_options()
+        local checkout_init_opts = luagit2_checkout.checkout_init_options()
         local tree_oid_string = "619f9935957e010c419cb9d15621916ddfcc0b96"
         local tree_oid = luagit2.oid_fromstr(tree_oid_string)
         local otype_tree = luagit2.object_string2type("tree")
         local obj_tree = luagit2.object_lookup(repo, tree_oid, otype_tree)
         
         -- Checkout the branch whose tree is this.
-        luagit2.checkout_tree(repo, obj_tree, checkout_init_opts)
+        luagit2_checkout.checkout_tree(repo, obj_tree, checkout_init_opts)
         local repo_HEAD_ref = luagit2.repository_head(repo)
         local ref_name = luagit2.reference_name(repo_HEAD_ref)
         -- In this case the tree belogs to master branch. Thus this assertion
