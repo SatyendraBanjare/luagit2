86da7be943fc452b797999df09f7d10b1881ca7f
Satyendra_Kumar_Banjare <satyendrabanjare99@gmail.com>
2018-08-11 12:50:04 +0530

    src(error handling): modified blame,blob,branch,buf

--- src/luagit2/branch/lua_branch.c
+++ src/luagit2/branch/lua_branch.c
@@ -1,10 +1,15 @@
 #include "lua_branch.h"
 
 int lua_git_branch_create (lua_State *L) {
+
+	if (lua_gettop(L) != 4) {
+        return luaL_error(L, "expecting 4 argument(s) : luagit2_repository,branch_name(string),luagit2_commit,force(int)");
+    }
+
 	luagit2_reference *lua_ref;
-	const luagit2_repository *lua_repo = (luagit2_repository *)lua_touserdata(L, 1);
+	const luagit2_repository *lua_repo = (luagit2_repository *)luaL_checkudata(L, 1,"luagit2_repository");
 	const char *branch_name = luaL_checkstring(L, 2);
-	const luagit2_commit *lua_commit = (luagit2_commit *)lua_touserdata(L, 3);
+	const luagit2_commit *lua_commit = (luagit2_commit *)luaL_checkudata(L, 3,"luagit2_commit");
 	const int force = luaL_checkinteger(L, 4);
 
 	lua_ref = (luagit2_reference *)lua_newuserdata(L, sizeof(*lua_ref));
@@ -23,10 +28,15 @@ int lua_git_branch_create (lua_State *L) {
 }
 
 int lua_git_branch_create_from_annotated (lua_State *L) {
+
+	if (lua_gettop(L) != 4) {
+        return luaL_error(L, "expecting 4 argument(s) : luagit2_repository,branch_name(string),luagit2_annotated_commit,force(int)");
+    }
+
 	luagit2_reference *lua_ref;
-	const luagit2_repository *lua_repo = (luagit2_repository *)lua_touserdata(L, 1);
+	const luagit2_repository *lua_repo = (luagit2_repository *)luaL_checkudata(L, 1,"luagit2_repository");
 	const char *branch_name = luaL_checkstring(L, 2);
-	const luagit2_annotated_commit *lua_annotated_commit = (luagit2_annotated_commit *)lua_touserdata(L, 3);
+	const luagit2_annotated_commit *lua_annotated_commit = (luagit2_annotated_commit *)luaL_checkudata(L, 3,"luagit2_annotated_commit");
 	const int force = luaL_checkinteger(L, 4);
 
 	lua_ref = (luagit2_reference *)lua_newuserdata(L, sizeof(*lua_ref));
@@ -45,29 +55,49 @@ int lua_git_branch_create_from_annotated (lua_State *L) {
 }
 
 int lua_git_branch_delete (lua_State *L) {
-	const luagit2_reference *lua_ref_branch = (luagit2_reference *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 1) {
+        return luaL_error(L, "expecting 1 argument(s) : luagit2_reference");
+    }
+
+	const luagit2_reference *lua_ref_branch = (luagit2_reference *)luaL_checkudata(L, 1,"luagit2_reference");
 	check_error_long(git_branch_delete(lua_ref_branch->reference), "Error deleting the branch", NULL);
 	return 1;
 }
 
 int lua_git_branch_is_checked_out (lua_State *L) {
-	const luagit2_reference *lua_ref_branch = (luagit2_reference *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 1) {
+        return luaL_error(L, "expecting 1 argument(s) : luagit2_reference");
+    }
+
+	const luagit2_reference *lua_ref_branch = (luagit2_reference *)luaL_checkudata(L, 1,"luagit2_reference");
 	int is_checked_out = git_branch_is_checked_out(lua_ref_branch->reference);
 	lua_pushboolean(L, is_checked_out);
 	return 1;
 }
 
 int lua_git_branch_is_head (lua_State *L) {
-	const luagit2_reference *lua_ref_branch = (luagit2_reference *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 1) {
+        return luaL_error(L, "expecting 1 argument(s) : luagit2_reference");
+    }
+
+	const luagit2_reference *lua_ref_branch = (luagit2_reference *)luaL_checkudata(L, 1,"luagit2_reference");
 	int is_head = git_branch_is_head(lua_ref_branch->reference);
 	lua_pushboolean(L, is_head);
 	return 1;
 }
 
 int lua_git_branch_iterator_new (lua_State *L) {
+
+	if (lua_gettop(L) != 2 ) {
+        return luaL_error(L, "expecting 2 argument(s) : luagit2_repository,luagit2_branch_type");
+    }
+
 	luagit2_branch_iterator *lua_branch_iterator;
-	const luagit2_repository *lua_repo = (luagit2_repository *)lua_touserdata(L, 1);
-	const luagit2_branch_type *list_flags = (luagit2_branch_type *)lua_touserdata(L, 2);
+	const luagit2_repository *lua_repo = (luagit2_repository *)luaL_checkudata(L, 1,"luagit2_repository");
+	const luagit2_branch_type *list_flags = (luagit2_branch_type *)luaL_checkudata(L, 2,"luagit2_branch_type");
 
 	lua_branch_iterator = (luagit2_branch_iterator *)lua_newuserdata(L, sizeof(*lua_branch_iterator));
 	lua_branch_iterator->branch_iterator  = NULL;
@@ -84,10 +114,15 @@ int lua_git_branch_iterator_new (lua_State *L) {
 }
 
 int lua_git_branch_lookup (lua_State *L) {
+
+	if (lua_gettop(L) != 3 ) {
+        return luaL_error(L, "expecting 3 argument(s) : luagit2_repository,branch_name(string)luagit2_branch_type");
+    }
+
 	luagit2_reference *lua_ref;
-	const luagit2_repository *lua_repo = (luagit2_repository *)lua_touserdata(L, 1);
+	const luagit2_repository *lua_repo = (luagit2_repository *)luaL_checkudata(L, 1,"luagit2_repository");
 	const char *branch_name = luaL_checkstring(L, 2);
-	const luagit2_branch_type *lua_branch_type = (luagit2_branch_type *)lua_touserdata(L, 3);
+	const luagit2_branch_type *lua_branch_type = (luagit2_branch_type *)luaL_checkudata(L, 3,"luagit2_branch_type");
 
 	lua_ref = (luagit2_reference *)lua_newuserdata(L, sizeof(*lua_ref));
 	lua_ref->reference  = NULL;
@@ -105,8 +140,13 @@ int lua_git_branch_lookup (lua_State *L) {
 }
 
 int lua_git_branch_move (lua_State *L) {
+
+	if (lua_gettop(L) != 3 ) {
+        return luaL_error(L, "expecting 3 argument(s) : luagit2_reference,new_branch_name(string),force(int)");
+    }
+
 	luagit2_reference *lua_ref;
-	const luagit2_reference *lua_branch_from = (luagit2_reference *)lua_touserdata(L, 1);
+	const luagit2_reference *lua_branch_from = (luagit2_reference *)luaL_checkudata(L, 1,"luagit2_reference");
 	const char *new_branch_name = luaL_checkstring(L, 2);
 	const int force = luaL_checkinteger(L, 3);
 
@@ -126,7 +166,12 @@ int lua_git_branch_move (lua_State *L) {
 }
 
 int lua_git_branch_name (lua_State *L) {
-	const luagit2_reference *lua_branch_from = (luagit2_reference *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 1 ) {
+        return luaL_error(L, "expecting 1 argument(s) : luagit2_reference");
+    }
+
+	const luagit2_reference *lua_branch_from = (luagit2_reference *)luaL_checkudata(L, 1,"luagit2_reference");
 	const char *branch_name = NULL;
 	check_error_long(git_branch_name(&branch_name, lua_branch_from->reference), "Failed to get the branch name", NULL);
 	lua_pushstring(L, branch_name);
@@ -134,9 +179,14 @@ int lua_git_branch_name (lua_State *L) {
 }
 
 int lua_git_branch_next (lua_State *L) {
+
+	if (lua_gettop(L) != 2 ) {
+        return luaL_error(L, "expecting 2 argument(s) : luagit2_branch_type,luagit2_branch_iterator");
+    }
+
 	luagit2_reference *lua_ref;
-	luagit2_branch_type *lua_branch_t = (luagit2_branch_type *)lua_touserdata(L, 1);
-	luagit2_branch_iterator *lua_branch_iterator = (luagit2_branch_iterator *)lua_touserdata(L, 2);
+	luagit2_branch_type *lua_branch_t = (luagit2_branch_type *)luaL_checkudata(L, 1,"luagit2_branch_type");
+	luagit2_branch_iterator *lua_branch_iterator = (luagit2_branch_iterator *)luaL_checkudata(L, 2,"luagit2_branch_iterator");
 
 	lua_ref = (luagit2_reference *)lua_newuserdata(L, sizeof(*lua_ref));
 	lua_ref->reference  = NULL;
@@ -154,7 +204,12 @@ int lua_git_branch_next (lua_State *L) {
 }
 
 int lua_git_branch_set_upstream (lua_State *L) {
-	const luagit2_reference *lua_branch_from = (luagit2_reference *)lua_touserdata(L, 1);
+
+	if (lua_gettop(L) != 2 ) {
+        return luaL_error(L, "expecting 2 argument(s) : luagit2_reference,upstream_name(string)");
+    }
+
+	const luagit2_reference *lua_branch_from = (luagit2_reference *)luaL_checkudata(L, 1,"luagit2_reference");
 	const char *upstream_name = luaL_checkstring(L, 2);
 	check_error_long(git_branch_set_upstream(lua_branch_from->reference, upstream_name),
 	    "Failed to set upstream for given branch", NULL);
@@ -162,8 +217,13 @@ int lua_git_branch_set_upstream (lua_State *L) {
 }
 
 int lua_git_branch_upstream (lua_State *L) {
+
+	if (lua_gettop(L) != 1 ) {
+        return luaL_error(L, "expecting 1 argument(s) : luagit2_reference");
+    }
+
 	luagit2_reference *lua_ref;
-	const luagit2_reference *lua_branch_base = (luagit2_reference *)lua_touserdata(L, 1);
+	const luagit2_reference *lua_branch_base = (luagit2_reference *)luaL_checkudata(L, 1,"luagit2_reference");
 
 	lua_ref = (luagit2_reference *)lua_newuserdata(L, sizeof(*lua_ref));
 	lua_ref->reference  = NULL;
--- src/luagit2/buf/lua_buf_help.c
+++ src/luagit2/buf/lua_buf_help.c
@@ -1,6 +1,13 @@
 #include "lua_buf_help.h"
 
 int lua_git_buf_details(lua_State *L) {
+
+	if (lua_gettop(L) != 1 ) {
+        return luaL_error(L, "expecting 1 argument(s) : luagit2_buf");
+    }
+
+    // We have not used luaL_checkdata here Because it was causing errors.
+
 	const luagit2_buf *Buffer = (luagit2_buf *)lua_touserdata(L, 1);
 	const char *String_Value = (char *)((Buffer->buf)->ptr);
 	lua_pushstring(L, String_Value);
