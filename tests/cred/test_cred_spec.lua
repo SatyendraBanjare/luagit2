5fa5124c18080d5ba8e23f58b18813d90c146e10
Satyendra_Kumar_Banjare <satyendrabanjare99@gmail.com>
2018-08-11 22:11:14 +0530

    tests(cred): modified for test coverage

--- tests/cred/test_cred_spec.lua
+++ tests/cred/test_cred_spec.lua
@@ -2,6 +2,7 @@
 
 describe(" Cred Methods Tests ", function()
 	local luagit2 = require("luagit2")
+	local luagit2_cred = require("cred.cred_cover")
 
 	setup(function()
 		luagit2.init()
@@ -13,27 +14,27 @@ describe(" Cred Methods Tests ", function()
 
 	it("Tests Cred Username ",function()
 		-- Create a git username type cred and test its validity.
-		local cred_username_new = luagit2.cred_username_new("UserName")
-		local value = luagit2.cred_username_details(cred_username_new)
+		local cred_username_new = luagit2_cred.cred_username_new("UserName")
+		local value = luagit2_cred.cred_username_details(cred_username_new)
 		assert.are.equal("UserName",value)
-		luagit2.cred_free(cred_username_new)
+		luagit2_cred.cred_free(cred_username_new)
 	end)
 
 	it("Tests Cred Userpass_Plaintext ",function()
 		-- Create a git userpass type cred and test its validity.
-		local cred_userpass_new = luagit2.cred_userpass_plaintext_new("UserName","Password@123")
-		local username , password = luagit2.cred_userpass_details(cred_userpass_new)
+		local cred_userpass_new = luagit2_cred.cred_userpass_plaintext_new("UserName","Password@123")
+		local username , password = luagit2_cred.cred_userpass_details(cred_userpass_new)
 		assert.are.equal("UserName",username)
 		assert.are.equal("Password@123",password)
-		luagit2.cred_free(cred_userpass_new)
+		luagit2_cred.cred_free(cred_userpass_new)
 	end)
 
 	it("Tests Cred SSH_key agent ",function()
 		-- Create a git ssh_from_agent type cred and test its validity.
-		local cred_ssh_key_agent_new = luagit2.cred_ssh_key_from_agent("AGENT_NAME")
-		local value = luagit2.cred_ssh_key_from_agent_details(cred_ssh_key_agent_new)
+		local cred_ssh_key_agent_new = luagit2_cred.cred_ssh_key_from_agent("AGENT_NAME")
+		local value = luagit2_cred.cred_ssh_key_from_agent_details(cred_ssh_key_agent_new)
 		assert.are.equal("AGENT_NAME",value)
-		luagit2.cred_free(cred_ssh_key_agent_new)
+		luagit2_cred.cred_free(cred_ssh_key_agent_new)
 	end)
 
 end)
