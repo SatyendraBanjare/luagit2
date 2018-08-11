f635b1633725d4cdee4578ccf0601a97cb2e6db8
Satyendra_Kumar_Banjare <satyendrabanjare99@gmail.com>
2018-08-11 20:10:42 +0530

    tests(commit): modified tests for coverage

--- tests/commit/test_commit_spec.lua
+++ tests/commit/test_commit_spec.lua
@@ -4,6 +4,7 @@ local fixer = require("Fixtures.fix_repo")
 
 describe(" Commit Methods Tests ", function()
 	local luagit2 = require("luagit2")
+	local luagit2_commit = require("commit.commit_cover")
 	local repo
 	local commit_id_a_string = "108ddee361877aa5c044d89d8dd232b8fd0f8992"
 	local commit_id_b_string = "36060c58702ed4c2a40832c51758d5344201d89a"
@@ -21,17 +22,17 @@ describe(" Commit Methods Tests ", function()
 		repo = luagit2.repository_open("Fixtures/WORKON_REPO/.git")
 		commit_id_a = luagit2.oid_fromstr(commit_id_a_string)
 		commit_id_b = luagit2.oid_fromstr(commit_id_b_string)
-		commit_a = luagit2.commit_lookup(repo,commit_id_a)
-		commit_b = luagit2.commit_lookup(repo,commit_id_b)
-		author_sign_a = luagit2.commit_author(commit_a)
-		author_sign_b = luagit2.commit_author(commit_b)
-		committer_sign_a = luagit2.commit_committer(commit_a)
-		committer_sign_b = luagit2.commit_committer(commit_b)
+		commit_a = luagit2_commit.commit_lookup(repo,commit_id_a)
+		commit_b = luagit2_commit.commit_lookup(repo,commit_id_b)
+		author_sign_a = luagit2_commit.commit_author(commit_a)
+		author_sign_b = luagit2_commit.commit_author(commit_b)
+		committer_sign_a = luagit2_commit.commit_committer(commit_a)
+		committer_sign_b = luagit2_commit.commit_committer(commit_b)
 	end)
 
 	after_each(function()
-		luagit2.commit_free(commit_a)
-		luagit2.commit_free(commit_b)
+		luagit2_commit.commit_free(commit_a)
+		luagit2_commit.commit_free(commit_b)
 		luagit2.repository_free(repo)
 		fixer.set_back()
 	end)
@@ -119,66 +120,66 @@ describe(" Commit Methods Tests ", function()
 	end)
 
 	it("Tests commit Body",function()
-		assert.are.equal("Commit message body",luagit2.commit_body(commit_a))
-		assert.is_nil(luagit2.commit_body(commit_b))
+		assert.are.equal("Commit message body",luagit2_commit.commit_body(commit_a))
+		assert.is_nil(luagit2_commit.commit_body(commit_b))
 	end)
 
 	it("Tests extract commit Header Field ", function()
 		-- Parent for commit_a is commit_b
 		-- So the commit id of commit_b 
 		-- Should be received
-		local parent_buf = luagit2.commit_header_field(commit_a,"parent")
+		local parent_buf = luagit2_commit.commit_header_field(commit_a,"parent")
 		assert.are.equal(commit_id_b_string, luagit2.buf_details(parent_buf))
 
 		-- We should get the current write-tree id
-		local tree_buf = luagit2.commit_header_field(commit_a,"tree")
+		local tree_buf = luagit2_commit.commit_header_field(commit_a,"tree")
 		assert.are.equal(current_write_tree_id, luagit2.buf_details(tree_buf))
 	end)
 
 	it("Tests commit lookup using prefix", function()
 		-- Create a simple commit object
-		local Commit_test = luagit2.commit_lookup(repo,commit_id_a)
+		local Commit_test = luagit2_commit.commit_lookup(repo,commit_id_a)
 
 		-- If this commit is valid , it should
 		-- be same as commit_a. Lets check for
 		-- its id.
 
-		local Commit_test_id = luagit2.commit_id(Commit_test)
+		local Commit_test_id = luagit2_commit.commit_id(Commit_test)
 		assert.are.equal(commit_id_a_string, luagit2.oid_tostr_s(Commit_test_id))
-		luagit2.commit_free(Commit_test)
+		luagit2_commit.commit_free(Commit_test)
 	end)
 
 	it("Tests commit Message",function()
-		local message = luagit2.commit_message(commit_b)
+		local message = luagit2_commit.commit_message(commit_b)
 		assert.are.same("subdirectories\n",message)
 	end)
 	
 	it("Tests commit Message encoding ",function()
 		-- Currently the messages are not encoded at all
 		-- we therefore receive a nil
-		local message_encoding = luagit2.commit_message_encoding(commit_a)
+		local message_encoding = luagit2_commit.commit_message_encoding(commit_a)
 		assert.are.same(nil,message)
 	end)
 
 	it("Tests Commit Message Raw", function()
-		local message_raw = luagit2.commit_message(commit_b)
+		local message_raw = luagit2_commit.commit_message(commit_b)
 		assert.are.same("subdirectories\n",message_raw)
 	end)
 
 	it("Tests Commit Nth generation parent",function()
 		-- Zero generation ancestor means the commit itself
-		local commit_gen_zero = luagit2.commit_nth_gen_ancestor(commit_a,0)
+		local commit_gen_zero = luagit2_commit.commit_nth_gen_ancestor(commit_a,0)
 		-- Check if we got the same commit or not
-		local commit_gen_zero_id = luagit2.commit_id(commit_gen_zero)
+		local commit_gen_zero_id = luagit2_commit.commit_id(commit_gen_zero)
 		assert.are.equal(commit_id_a_string, luagit2.oid_tostr_s(commit_gen_zero_id))
 
 		-- generation one means immediate parent commit
-		local commit_gen_one = luagit2.commit_nth_gen_ancestor(commit_a,1)
+		local commit_gen_one = luagit2_commit.commit_nth_gen_ancestor(commit_a,1)
 		-- Check for values
-		local commit_gen_one_id = luagit2.commit_id(commit_gen_one)
+		local commit_gen_one_id = luagit2_commit.commit_id(commit_gen_one)
 		assert.are.equal(commit_id_b_string, luagit2.oid_tostr_s(commit_gen_one_id))
-		luagit2.commit_free(commit_gen_zero)
-		luagit2.commit_free(commit_gen_one)
+		luagit2_commit.commit_free(commit_gen_zero)
+		luagit2_commit.commit_free(commit_gen_one)
 	end)
 
 	it("Tests Commit Parent",function()
@@ -186,11 +187,11 @@ describe(" Commit Methods Tests ", function()
 		-- the values should match accordingly
 		-- the param value of 0 means we want 
 		-- just immediate before value of commit.
-		local parent_commit_one = luagit2.commit_parent(commit_a,0)
+		local parent_commit_one = luagit2_commit.commit_parent(commit_a,0)
 
-		local parent_commit_id = luagit2.commit_id(parent_commit_one)
+		local parent_commit_id = luagit2_commit.commit_id(parent_commit_one)
 		assert.are.equal(commit_id_b_string, luagit2.oid_tostr_s(parent_commit_id))
-		luagit2.commit_free(parent_commit_one)
+		luagit2_commit.commit_free(parent_commit_one)
 	end)
 
 	it("Tests commit Parent ID",function()
@@ -198,7 +199,7 @@ describe(" Commit Methods Tests ", function()
 		-- the values should match accordingly
 		-- the param value of 0 means we want 
 		-- just immediate before value of commit.
-		local parent_commit_id = luagit2.commit_parent_id(commit_a,0)
+		local parent_commit_id = luagit2_commit.commit_parent_id(commit_a,0)
 		assert.are.equal(commit_id_b_string, luagit2.oid_tostr_s(parent_commit_id))
 	end)
 
@@ -206,7 +207,7 @@ describe(" Commit Methods Tests ", function()
 		-- Since the commit A has been create using
 		-- only one parent commit, We should get 1 as
 		-- its parent count 
-		local parent_count = luagit2.commit_parentcount(commit_a)
+		local parent_count = luagit2_commit.commit_parentcount(commit_a)
 		assert.are.equal(1,parent_count)
 	end)
 
@@ -215,17 +216,17 @@ describe(" Commit Methods Tests ", function()
 						"parent 36060c58702ed4c2a40832c51758d5344201d89a\n" ..
 						"author test_user <test@example.com> 1530366649 +0530\n" .. 
 						"committer test_user <test@example.com> 1530366649 +0530\n"
-		local Header_A = luagit2.commit_raw_header(commit_a)
+		local Header_A = luagit2_commit.commit_raw_header(commit_a)
 		assert.are.equal(Header,Header_A)
 	end)
 
 	it("Tests Commit Summary", function()
-		local commit_a_summary = luagit2.commit_summary(commit_a)
+		local commit_a_summary = luagit2_commit.commit_summary(commit_a)
 		assert.are.equal("Added File",commit_a_summary)
 	end)
 
 	it("Tests Commit Tree", function()
-		local commit_a_tree = luagit2.commit_tree(commit_a)
+		local commit_a_tree = luagit2_commit.commit_tree(commit_a)
 		local tree_id = luagit2.tree_id(commit_a_tree)
 
 		assert.are.equal(current_write_tree_id, luagit2.oid_tostr_s(tree_id))
@@ -234,7 +235,7 @@ describe(" Commit Methods Tests ", function()
 
 	it("Tests Commit Tree Id",function()
 		-- Please refer luagit2.it2's original documentation for this
-		local tree_id = luagit2.commit_tree_id(commit_a)
+		local tree_id = luagit2_commit.commit_tree_id(commit_a)
 		assert.are.equal(current_write_tree_id, luagit2.oid_tostr_s(tree_id))
 	end)
 
@@ -244,16 +245,16 @@ describe(" Commit Methods Tests ", function()
 		local parent_tree = luagit2.tree_lookup(repo,parent_tree_id)
 
 		-- Create a new Commit and get commit Id
-		local new_commit_id =  luagit2.commit_create_update_head(repo,author_sign_a,
+		local new_commit_id =  luagit2_commit.commit_create_update_head(repo,author_sign_a,
 			committer_sign_a,"New Commit From luagit2",parent_tree,commit_a)
 
 		local new_commit_string = luagit2.oid_tostr_s(new_commit_id)
 		
 		-- Try to lookup that Id in repo
 		-- A test of whether the commit was actually created or not
-		local new_commit = luagit2.commit_lookup(repo,new_commit_id)
+		local new_commit = luagit2_commit.commit_lookup(repo,new_commit_id)
 		-- Check the lookedup Commit is the one that we created
-		local lookedup_commit_id = luagit2.commit_id(new_commit)
+		local lookedup_commit_id = luagit2_commit.commit_id(new_commit)
 
 		assert.are.equal(new_commit_string,luagit2.oid_tostr_s(lookedup_commit_id))
 
@@ -265,16 +266,16 @@ describe(" Commit Methods Tests ", function()
 		local parent_tree = luagit2.tree_lookup(repo,parent_tree_id)
 
 		-- Create a new Commit and get commit Id
-		local new_commit_id =  luagit2.commit_create_update_none(repo,author_sign_a,
+		local new_commit_id =  luagit2_commit.commit_create_update_none(repo,author_sign_a,
 			committer_sign_a,"New Commit From luagit2",parent_tree,commit_a)
 
 		local new_commit_string = luagit2.oid_tostr_s(new_commit_id)
 		
 		-- Try to lookup that Id in repo
 		-- A test of whether the commit was actually created or not
-		local new_commit = luagit2.commit_lookup(repo,new_commit_id)
+		local new_commit = luagit2_commit.commit_lookup(repo,new_commit_id)
 		-- Check the lookedup Commit is the one that we created
-		local lookedup_commit_id = luagit2.commit_id(new_commit)
+		local lookedup_commit_id = luagit2_commit.commit_id(new_commit)
 
 		assert.are.equal(new_commit_string,luagit2.oid_tostr_s(lookedup_commit_id))
 
@@ -307,14 +308,14 @@ describe(" Commit Methods Tests ", function()
 		\n\
 		a simple commit which works\n";
 
-		local new_commit_id = luagit2.commit_create_with_signature(repo,data,gpgsig)
-		local sign_buf, extra_buf = luagit2.commit_extract_signature(repo,new_commit_id)
+		local new_commit_id = luagit2_commit.commit_create_with_signature(repo,data,gpgsig)
+		local sign_buf, extra_buf = luagit2_commit.commit_extract_signature(repo,new_commit_id)
 
 		assert.are.equal(gpgsig,luagit2.buf_details(sign_buf))
 	end)
 
 	it("Tests commit time",function()
-		local time_str = luagit2.commit_time(commit_a)
+		local time_str = luagit2_commit.commit_time(commit_a)
 		assert.is_not_nil(time_str)
 		assert.is_string(time_str)
 	end)
