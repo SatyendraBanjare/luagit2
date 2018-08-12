dc7c0db3306f54eeb8a0442b33c5e456b037bb70
Satyendra_Kumar_Banjare <satyendrabanjare99@gmail.com>
2018-08-12 09:48:25 +0530

    Tests new coverage

---
+++ install_libgit.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd ~
+git clone --depth=1 -b maint/v0.27 https://github.com/libgit2/libgit2.git
+cd libgit2/
+mkdir build && cd build
+cmake .. -DBUILD_CLAR=OFF
+sudo make install
+ls -la ..
